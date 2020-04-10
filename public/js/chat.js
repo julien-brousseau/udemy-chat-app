b3cae0c62bb6a22abdc8a7f655bc79dff0d266d1
djoolien <djoolien@gmail.com>
2020-04-10 15:06:37 -0400

    Added geolocation

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,32 @@
-io();
+// Websocket object
+const socket = io();
+
+// Client recieves the server event sent with socket.emit()
+socket.on("message", (m) => {
+  console.log(m);
+});
+
+// Attach emit event to the form submit
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msg = e.target.elements.message.value;
+  socket.emit("sendMessage", msg);
+})
+
+// Share geolocalisation
+document.querySelector("#send-location").addEventListener("click", () => {
+
+  // No browser support
+  if (!navigator.geolocation) {
+    return alert("Unable to access geolocation");
+  }
+
+  // Send latitude and longitude to server
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit("sendLocation", {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  });
+
+});
--- src/index.js
+++ src/index.js
@@ -19,8 +19,32 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 // Listen for connections
-io.on("connection", () => {
-  console.log("New web socket connection");
+io.on("connection", (socket) => {
+
+  console.log("New socket connection");
+
+  // Server -> Single connected
+  socket.emit("message", "Welcome!");
+
+  // Server -> All active connections except the current one
+  socket.broadcast.emit("message", "A new user has joined");
+
+  // Server -> All active connections
+  socket.on("sendMessage", (msg) => {
+    io.emit("message", msg);
+  });
+
+  // Recieve latitude and longitude from client, then publish location to all clients
+  socket.on("sendLocation", (coords) => {
+    io.emit("message", "https://google.com/maps?q=" + coords.latitude + "," + coords.longitude);
+  });
+
+  // When a single connection closes
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left")
+    console.log();
+  });
+
 })
 
 // Launch server with port from env file

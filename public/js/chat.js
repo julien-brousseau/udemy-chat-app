464cc15edb90badba60dd387d8ff84d3faf213bb
djoolien <djoolien@gmail.com>
2020-04-10 21:12:12 -0400

    Added autoscroll

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,6 +17,33 @@ const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 // Get the current URL data using the QueryString library (loaded as script)
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
+//
+const autoScroll = () => {
+
+  // Get the new message element
+  const $newMsg = $messages.lastElementChild;
+
+  // Get the height of the message + margin
+  const newMsgStyles = getComputedStyle($newMsg);
+  const newMessageMargin = parseInt(newMsgStyles.marginBottom)
+  const newMessageHeight = $newMsg.offsetHeight + newMessageMargin;
+
+  // Visible height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Messages container height
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled so far
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  // Check if we're scrolled down without the new message
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+
+}
+
 // Server responses
 
 // Client recieves the server event sent with socket.emit()
@@ -31,6 +58,7 @@ socket.on("message", (message) => {
 
   // Insert added html into the div
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
 
 });
 
@@ -42,6 +70,7 @@ socket.on("locationMessage", (message) => {
       createdAt: moment(message.createdAt).format("H:mm")
     });
     $messages.insertAdjacentHTML("beforeend", html);
+    autoScroll();
 });
 
 // Refresh users list
